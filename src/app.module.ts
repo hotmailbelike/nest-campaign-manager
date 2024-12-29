4061fd6c7272eae8966bd5e9b2d579333410a7a6
Abdullah Omar <mdabdullahomar97@gmail.com>
2024-12-29 19:05:29 +0600

    chore: prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CampaignsModule } from './campaigns/campaigns.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CampaignsModule],
+  imports: [CampaignsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
