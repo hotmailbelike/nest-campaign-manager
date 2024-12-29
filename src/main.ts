ed0cf99323eb2f81b8ccf7e0d08287f462c50a4a
Abdullah Omar <mdabdullahomar97@gmail.com>
2024-12-29 22:04:24 +0600

    chore: enable cors anywhere

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || '8000';
 
+  // This just for dev purposes, in real life would have to be more restrictive
+  app.enableCors({
+    origin: '*', // This allows requests from any origin
+  });
+
   await app.listen(PORT, () => {
     console.info('nest campaign manager started on PORT:', PORT);
   });
